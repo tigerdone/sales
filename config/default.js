0d76cd6c502f8650ddb334f64767b23deac89adc
tiger <lihu@codoon.com>
2020-07-13 00:41:14 +0800

    add

--- config/default.js
+++ config/default.js
@@ -5,7 +5,7 @@ module.exports = {
         key: 'tiger',
         maxAge: 2592000000
     },
-    mongodb:"127.0.0.1:27017/sales",
+    mongodb:"mongodb://127.0.0.1:27017/sales",
     // mongodb: 'mongodb://tigerdone:18328646311lihu@ds131942.mlab.com:31942/tigerdone',
     mongodbDev: 'mongodb://tigerdone:18328646311lihu@ds249967.mlab.com:49967/sales'
 };
